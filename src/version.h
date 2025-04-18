431eb89972b53777bd9c8bde5c0003251e2edceb
sheverdin <sheverdin@fort-telecom.ru>
2025-04-18 16:25:35 +0500

    version changed

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #define APP_NAME "tf_snmp"
-#define APP_VERSION_MAJOR (5)
-#define APP_VERSION_MINOR (4)
-#define APP_VERSION_PATCH (3)
-#define APP_RELEASE ("5.4.3")
-#define FULL_VERSION "5-4"
+#define APP_VERSION_MAJOR (1)
+#define APP_VERSION_MINOR (2)
+#define APP_VERSION_PATCH (0)
+#define APP_RELEASE ("1.2-0")
+#define FULL_VERSION "1-2"
